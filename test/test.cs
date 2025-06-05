6189a1eec54aa2311da5860123d02c01a8bdb705
kefir4iick <lizardm17@icloud.com>
2025-06-05 22:34:32 +0300

    фу

--- test/test.cs
+++ test/test.cs
@@ -3,27 +3,26 @@ using System.Diagnostics;
 using System.Linq;
 using System.Reflection; 
 using System.Threading;
-using System.Threading.Tasks;
 using Xunit;
 using AudioSynthApp;
 
 public class FpsLoopTests
 {
     [Fact]
-    public async Task FpsTest()
+    public void FpsTest()
     {
         var synth = new AudioSynthesizer();
         
         var audioOutput = synth.GetType()
                              .GetField("_audioOutput", BindingFlags.NonPublic | BindingFlags.Instance)
                              .GetValue(synth);
-    
+
         var realDriver = audioOutput?.GetType()
                                    .GetField("_waveOut", BindingFlags.NonPublic | BindingFlags.Instance)
                                    .GetValue(audioOutput)
                                    ?.GetType()
                                    .Name;
-    
+
         Console.WriteLine($"driver: {realDriver ?? "Unknown"}");
         
         const int targetFps = 60;
@@ -31,7 +30,7 @@ public class FpsLoopTests
         var frameTimes = new double[targetFps * 5]; 
         int frameCount = 0;
         var stopwatch = new Stopwatch();
-    
+
         var loop = new FpsBasedLoop(
             callback: () =>
             {
@@ -43,28 +42,28 @@ public class FpsLoopTests
             },
             targetFps: targetFps
         );
-    
+
         stopwatch.Start();
         loop.Start();
-        await Task.Delay(testDurationMs);
+        Thread.Sleep(testDurationMs);
         loop.Stop();
-    
+
         var relevantFrames = frameTimes
             .Skip(10)
             .Where(t => t > 0)
             .Take(frameCount - 10)
             .ToArray();
-    
+
         var fpsValues = relevantFrames.Select(t => 1000 / t).ToArray();
         var minFps = fpsValues.Min();
         var avgFps = fpsValues.Average();
         var maxFps = fpsValues.Max();
-    
+
         Console.WriteLine($"fps stats {testDurationMs} ms:");
         Console.WriteLine($"- average: {avgFps:F2}");
         Console.WriteLine($"- min: {minFps:F2}");
         Console.WriteLine($"- max: {maxFps:F2}");
-    
+
         Assert.InRange(avgFps, targetFps - 5, targetFps + 5);
     }
 }
